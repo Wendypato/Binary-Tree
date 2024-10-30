eefb568114bb41a8129ae484e0c64fa3cd0fa3a2
Wendy <152311301+Wendypato@users.noreply.github.com>
2024-10-30 16:59:49 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,35 @@ int main()
     tree.root = tree.insert(tree.root, 50);
     tree.root = tree.insert(tree.root, 30);
     tree.root = tree.insert(tree.root, 70);
+    tree.root = tree.insert(tree.root, 20);
+    tree.root = tree.insert(tree.root, 40);
+    tree.root = tree.insert(tree.root, 60);
+    tree.root = tree.insert(tree.root, 80);
 
+    cout << "In order traversal: ";
+    tree.inOrderTraversal(tree.root);
+    cout << endl;
+
+    cout << "Pre order traversal: ";
+    tree.preOrderTraversal(tree.root);
+    cout << endl;
+
+    cout << "Post order traversal: ";
+    tree.postOrderTraversal(tree.root);
+    cout << endl;
+
+    int valueToRemove = 30;
+    tree.root = tree.remove(tree.root, valueToRemove);
+    cout << "In order after deleting " << valueToRemove << ": ";
+    tree.inOrderTraversal(tree.root);
+    cout << endl;
+
+    //Ejemplo que vimos en clase
+    int valueToSearch = 30;
+    Node* result = tree.search(tree.root, valueToSearch);
+    cout<<result->data;
+
+    //Ejemplo visto en clase
     int valueToSearch = 30;
     Node* result = tree.search(tree.root, valueToSearch);
     cout<<result->data;
