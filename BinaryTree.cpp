d9f154f811e25b91b5a84b4f8688e6c051af1d4d
Wendy <152311301+Wendypato@users.noreply.github.com>
2024-10-30 16:57:48 -0600

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -48,5 +48,63 @@ Node* BinaryTree::search(Node* node, int value)
     }
     return search(node->right, value);
 }
+Node* BinaryTree::minValueNode(Node* node)
+{
+    Node* current = node;
+    while(current && current ->left != nullptr)
+    {
+        current = current->left;
+    }
+    return current;
+}
 
+Node* BinaryTree::remove(Node* node, int value) {
+    if (node == nullptr) return node;
+
+    if (value < node->data) {
+        node->left = remove(node->left, value);
+    } else if (value > node->data) {
+        node->right = remove(node->right, value);
+    } else {
+        //Nodo con un hijo o sin hijo
+        if (node->left == nullptr) {
+            Node* temp = node->right;
+            delete node;
+            return temp;
+        } else if (node->right == nullptr) {
+            Node* temp = node->left;
+            delete node;
+            return temp;
+        }
+
+        //Nodo con dos hiojs
+        Node* temp = minValueNode(node->right);
+        node->data = temp->data;
+        node->right = remove(node->right, temp->data);
+    }
+    return node;
+}
+
+void BinaryTree::inOrderTraversal(Node* node) {
+    if (node == nullptr) return;
+    inOrderTraversal(node->left);
+    cout << node->data << " ";
+    inOrderTraversal(node->right);
+}
+
+
+void BinaryTree::preOrderTraversal(Node* node) {
+    if (node == nullptr) return;
+    cout << node->data << " ";
+    preOrderTraversal(node->left);
+    preOrderTraversal(node->right);
+}
+
+
+void BinaryTree::postOrderTraversal(Node* node) {
+    if (node == nullptr) return;
+    postOrderTraversal(node->left);
+    postOrderTraversal(node->right);
+    cout << node->data << " ";
+}
 
